d1a71ba28921f82d98376ef1f6f2fbe1d1591895
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-10 18:04:40 +0000

    Add files via upload

---
+++ api/key.py
@@ -0,0 +1,5 @@
+KEY = "633f43ca5566dc20c8ba22cb10668f193a381cb8"
+
+## Client Secret: "633f43ca5566dc20c8ba22cb10668f193a381cb8"
+## Client ID: "201696"
+## Access Token: "ca89817a878d0289e82f2d69a3f9660a7d28b4dc"
