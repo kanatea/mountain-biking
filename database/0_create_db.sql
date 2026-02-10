ca86eb63590851c7c5e156f2a5016859a2ac28e4
camchalms <camchalms@gmail.com>
2026-02-10 18:44:54 +0000

    Update 0_create_db.sql

---
+++ database/0_create_db.sql
@@ -0,0 +1,21 @@
+CREATE TABLE trails (
+    id               SERIAL PRIMARY KEY,
+    strava_segment_id BIGINT UNIQUE,
+    name             TEXT NOT NULL,
+    distance_m       INTEGER,
+    elevation_gain_m INTEGER,
+    average_grade    NUMERIC,
+    start_point      TEXT,
+    end_point        TEXT
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS trail_ratings (
+	id SERIAL PRIMARY KEY,
+	trail_id INTEGER REFERENCES trails(id) ON DELETE CASCADE,
+	rating INTEGER CHECK (rating BETWEEN 1 AND 5),
+	comment TEXT,
+	created_at TIMESTAMP DEFAULT NOW()
+);
