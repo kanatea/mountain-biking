5563e10b4ad537a6a5d67d1e0fe9a1e1afb3a607
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-11 15:58:38 +0000

    Update 2_create_table.sql

--- database/2_create_table.sql
+++ database/2_create_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS public.trails (
+CREATE TABLE IF NOT EXISTS strava.trails (
 	id               SERIAL PRIMARY KEY,
     strava_segment_id BIGINT UNIQUE,
     name             TEXT NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS public.trails (
 );
 
 
-CREATE TABLE IF NOT EXISTS public.trail_ratings (
+CREATE TABLE IF NOT EXISTS pa.trail_ratings (
 	id SERIAL PRIMARY KEY,
 	trail_id INTEGER REFERENCES trails(id) ON DELETE CASCADE,
 	rating INTEGER CHECK (rating BETWEEN 1 AND 5),
@@ -18,13 +18,13 @@ CREATE TABLE IF NOT EXISTS public.trail_ratings (
 	created_at TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE IF NOT EXISTS public.users (
+CREATE TABLE IF NOT EXISTS pa.users (
 	id               SERIAL PRIMARY KEY,
     user_id 		 BIGINT UNIQUE,
     name             TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public.poi (
+CREATE TABLE IF NOT EXISTS pa.poi (
 	id               SERIAL PRIMARY KEY,
     poi_id 			 BIGINT UNIQUE,
     name             TEXT NOT NULL,
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS public.poi (
 	long			 TEXT
 );
 
-CREATE TABLE IF NOT EXISTS public.maintenance (
+CREATE TABLE IF NOT EXISTS pa.maintenance (
 	id               SERIAL PRIMARY KEY,
     feedback_id 	 BIGINT UNIQUE,
     trail_name       TEXT NOT NULL,
@@ -40,4 +40,5 @@ CREATE TABLE IF NOT EXISTS public.maintenance (
 	lat			  	 TEXT,
 	long			 TEXT
 );
+
 
