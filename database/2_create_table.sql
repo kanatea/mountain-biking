ed0937cf5c2ad9ed310dd5550de15f6761621fad
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-11 15:55:25 +0000

    Update 2_create_table.sql

--- database/2_create_table.sql
+++ database/2_create_table.sql
@@ -1,6 +1,4 @@
-DROP TABLE IF EXISTS public.trails;
-
-CREATE TABLE public.trails (
+CREATE TABLE IF NOT EXISTS public.trails (
 	id               SERIAL PRIMARY KEY,
     strava_segment_id BIGINT UNIQUE,
     name             TEXT NOT NULL,
@@ -18,4 +16,27 @@ CREATE TABLE IF NOT EXISTS public.trail_ratings (
 	rating INTEGER CHECK (rating BETWEEN 1 AND 5),
 	comment TEXT,
 	created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS public.users (
+	id               SERIAL PRIMARY KEY,
+    user_id 		 BIGINT UNIQUE,
+    name             TEXT NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS public.poi (
+	id               SERIAL PRIMARY KEY,
+    poi_id 			 BIGINT UNIQUE,
+    name             TEXT NOT NULL,
+	lat			  	 TEXT,
+	long			 TEXT
+);
+
+CREATE TABLE IF NOT EXISTS public.maintenance (
+	id               SERIAL PRIMARY KEY,
+    feedback_id 	 BIGINT UNIQUE,
+    trail_name       TEXT NOT NULL,
+	comment			 TEXT,
+	lat			  	 TEXT,
+	long			 TEXT
 );
