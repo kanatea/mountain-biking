8068b3a1de7da74bf384d0fb59772cbf62d895d7
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-11 14:59:46 +0000

    Update key.py

--- api/key.py
+++ api/key.py
@@ -2,4 +2,7 @@ KEY = "633f43ca5566dc20c8ba22cb10668f193a381cb8"
 
 ## Client Secret: "633f43ca5566dc20c8ba22cb10668f193a381cb8"
 ## Client ID: "201696"
-## Access Token: "ca89817a878d0289e82f2d69a3f9660a7d28b4dc"
+
+## Strava tokens
+### 'refresh_token': '410bb7b5c879a1b4aeb0f52cb55deaab9006d964'
+###'access_token': '7c7932203d34c8a597df4eb22cfa577fc743829e' 
