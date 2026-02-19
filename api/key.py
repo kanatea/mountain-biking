d3c45ce741c428cf2054bd65e6a926d94cf43e50
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-19 13:28:13 +0000

    Delete api directory

--- api/key.py
+++
@@ -1,8 +0,0 @@
-KEY = "633f43ca5566dc20c8ba22cb10668f193a381cb8"
-
-## Client Secret: "633f43ca5566dc20c8ba22cb10668f193a381cb8"
-## Client ID: "201696"
-
-## Strava tokens
-### 'refresh_token': '410bb7b5c879a1b4aeb0f52cb55deaab9006d964'
-###'access_token': '7c7932203d34c8a597df4eb22cfa577fc743829e' 
