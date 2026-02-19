6b3f6753d12b2000c4619622ee3cb14892de57c6
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-19 16:57:02 +0000

    Update 4_data_processing.sql

--- database/4_data_processing.sql
+++ database/4_data_processing.sql
@@ -2,15 +2,9 @@
 -- if the length of the climb (in meters) multiplied 
 -- by the grade of the climb is greater than 8,000.
 
-ALTER TABLE strava.trails
-DROP IF EXISTS climb_grade;
-
-ALTER TABLE strava.trails
-ADD climb_grade TEXT;
-
-Update strava.trails SET climb_grade = 'Not Categorized (Beginner)' WHERE climb_category=0;
-UPDATE strava.trails SET climb_grade = 'Easy' WHERE climb_category=1;
-UPDATE strava.trails SET climb_grade = 'Easy - Intermediate' WHERE climb_category=2;
-UPDATE strava.trails SET climb_grade = 'Intermediate - Difficult' WHERE climb_category=3;
-UPDATE strava.trails SET climb_grade = 'Difficult' WHERE climb_category=4;
-UPDATE strava.trails SET climb_grade = 'Expert (Hors Catégorie)' WHERE climb_category=5;
+Update strava.trails SET climb_category_desc = 'Not Categorized' WHERE climb_category=0;
+UPDATE strava.trails SET climb_category_desc = 'Category 4' WHERE climb_category=1;
+UPDATE strava.trails SET climb_category_desc = 'Category 3' WHERE climb_category=2;
+UPDATE strava.trails SET climb_category_desc = 'Category 2' WHERE climb_category=3;
+UPDATE strava.trails SET climb_category_desc = 'Category 1' WHERE climb_category=4;
+UPDATE strava.trails SET climb_category_desc = 'Hors Catégorie' WHERE climb_category=5;
