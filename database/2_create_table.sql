27ea0992e18f2847d65eca4a2a8e53ff0a887b19
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-20 14:51:34 +0000

    Update 2_create_table.sql

--- database/2_create_table.sql
+++ database/2_create_table.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS strava.trails;
 
-CREATE TABLE IF NOT EXISTS strava.trails
-(    id               SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS strava.trails(    
+    id               SERIAL PRIMARY KEY,
     strava_segment_id BIGINT UNIQUE,
-    name             TEXT NOT NULL,
+    name             varchar(255) NOT NULL,
+    -- can we make this the primary key
     region           TEXT DEFAULT 'Madeira',
     distance_m       INTEGER,
     elevation_gain_m INTEGER,
@@ -18,44 +19,5 @@ CREATE TABLE IF NOT EXISTS strava.trails
     polyline         TEXT
 );
 
-
-DROP TABLE IF EXISTS pa.users;
-
-CREATE TABLE IF NOT EXISTS pa.users (
-	id               SERIAL,
-    user_id 		 BIGINT UNIQUE,
-    username         varchar(255) NOT NULL PRIMARY KEY
-);
-
-DROP TABLE IF EXISTS pa.trails;
-
-CREATE TABLE IF NOT EXISTS pa.trails(
-	id               SERIAL,
-    trail_id 		 BIGINT UNIQUE NOT NULL PRIMARY KEY,
-    trail_name       TEXT
-);
-
-DROP TABLE IF EXISTS pa.trail_ratings;
-
-CREATE TABLE IF NOT EXISTS pa.trail_ratings (
-	id         SERIAL PRIMARY KEY,
-    trail_id   INTEGER NOT NULL REFERENCES pa.trails(trail_id),
-	rating      INTEGER CHECK (rating BETWEEN 1 AND 5),
-	username    varchar(255) NOT NULL REFERENCES pa.users(username),
-    comment     TEXT,
-	created_at TIMESTAMP DEFAULT NOW()
-);
-
-
-DROP TABLE IF EXISTS pa.maintenance;
-
-CREATE TABLE IF NOT EXISTS pa.maintenance (
-	id               SERIAL PRIMARY KEY,
-    feedback_id 	 BIGINT UNIQUE,
-    trail_id        INTEGER NOT NULL REFERENCES pa.trails(trail_id),
-	maint_comment	 TEXT,
-	lat			  	 TEXT,
-	long			 TEXT
-);
 
 
