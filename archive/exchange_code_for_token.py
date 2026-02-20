fd688e0b4e05c6233fc3fbf3ae12e95c0891ff7b
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-20 12:35:46 +0000

    Add files via upload

---
+++ archive/exchange_code_for_token.py
@@ -0,0 +1,25 @@
+import requests
+
+# FILL THESE IN CAREFULLY:
+CLIENT_ID = "201696"            # e.g. "12345" (from Strava API app page)
+CLIENT_SECRET = "633f43ca5566dc20c8ba22cb10668f193a381cb8"    # long secret string from the same page
+AUTH_CODE = "a82205bc0b7bc148927a5dc2a5f940762b58af47"  # your code
+REDIRECT_URI = "http://localhost/exchange_token"        # must match your app settings
+
+def main():
+    url = "https://www.strava.com/api/v3/oauth/token"
+    data = {
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "code": AUTH_CODE,
+        "grant_type": "authorization_code",
+        "redirect_uri": REDIRECT_URI,
+    }
+
+    print("Sending data:", data)  # for debugging
+    response = requests.post(url, data=data)
+    print("Status code:", response.status_code)
+    print("Response JSON:", response.json())
+
+if __name__ == "__main__":
+    main()
