c6fa105063d0e6d774148f1b8769b1f20fc347bd
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-22 15:16:16 +0000

    Update 3_create_table.sql

--- database/3_create_table.sql
+++ database/3_create_table.sql
@@ -1,19 +1,23 @@
-DROP TABLE IF EXISTS pa.users;
+-- Create tables for the pa schema, storing the information from the users, trail ratings/reviews, and any reported problems/maintenance issues
 
+DROP TABLE IF EXISTS pa.users;
+-- Stores user info
 CREATE TABLE IF NOT EXISTS pa.users (
 	id               SERIAL,
     username         varchar(255) NOT NULL PRIMARY KEY
 );
 
 DROP TABLE IF EXISTS pa.trails;
-
+-- Stores trail name that is reviewed or reported (limited to what exists from the strava dataset, 
+-- this constraint is reinforced from the frontend by limiting the input trail names
 CREATE TABLE IF NOT EXISTS pa.trails(
 	id               SERIAL,
     trail_name       varchar(255) NOT NULL PRIMARY KEY
 );
 
 DROP TABLE IF EXISTS pa.trail_ratings;
-
+-- Stores trail ratings, which includes a 1-5 star rating (stored as an integer), and a comment/review on the trail itself
+-- Timestamp to display the most recent comment
 CREATE TABLE IF NOT EXISTS pa.trail_ratings (
 	id         SERIAL PRIMARY KEY,
     trail_name   varchar(255) NOT NULL REFERENCES pa.trails(trail_name),
@@ -24,9 +28,13 @@ CREATE TABLE IF NOT EXISTS pa.trail_ratings (
 );
 
 DROP TABLE IF EXISTS pa.maintenance;
-
+-- Allows users to report any problems or maintenance issues on specific trails
+-- Timestamp to display the most recent problem
 CREATE TABLE IF NOT EXISTS pa.maintenance (
 	id               SERIAL PRIMARY KEY,
     trail_name      varchar(255) NOT NULL REFERENCES pa.trails(trail_name),
 	maint_comment	 varchar,
+	was_resolved     INTEGER (BETWEEN 0 AND 1),
+	created_at       TIMESTAMP DEFAULT NOW()
 );
+
