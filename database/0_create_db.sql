ee2428cde64874234efc35397bd538ddd3342cea
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-22 15:01:31 +0000

    Update 0_create_db.sql

--- database/0_create_db.sql
+++ database/0_create_db.sql
@@ -1,4 +1,4 @@
--- Database: madeira_trails
+-- For the purposes of this project, we will create a database named madeira_trails
 
 -- DROP DATABASE IF EXISTS madeira_trails;
 
@@ -6,9 +6,10 @@ CREATE DATABASE madeira_trails
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'English_United Kingdom.1252'
-    LC_CTYPE = 'English_United Kingdom.1252'
-    LOCALE_PROVIDER = 'libc'
+    LC_COLLATE = 'English_United Kingdom.1252' --replace with what is registered on your machine
+    LC_CTYPE = 'English_United Kingdom.1252'   --replace with what is registered on your machine
+    LOCALE_PROVIDER = 'libc'                   --replace with what is registered on your machine
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
+
