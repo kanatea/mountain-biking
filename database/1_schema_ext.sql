0a58448bb6a4b367639172a7ba861f3dda5200fd
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-22 15:03:19 +0000

    Update 1_schema_ext.sql

--- database/1_schema_ext.sql
+++ database/1_schema_ext.sql
@@ -1,13 +1,17 @@
--- SCHEMAS AND EXTENSION
+-- Creating schemas and adding extensions
+
+-- Not sure why we called it pa, but this schema will contain front-end user-specific interactions
 CREATE SCHEMA IF NOT EXISTS pa
   AUTHORIZATION postgres;
 
+-- This schema will contain data imported from the strava api
 CREATE SCHEMA IF NOT EXISTS strava
   AUTHORIZATION postgres;
 
--- PostGIS Extension
+-- PostGIS Extensions
 CREATE EXTENSION postgis;
 CREATE EXTENSION pgrouting;
+
 
 
 
