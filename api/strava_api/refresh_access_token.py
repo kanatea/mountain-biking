ce5af8e5a7ab8cc65629c926dbaadd53ca56523e
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-22 16:01:43 +0000

    Update refresh_access_token.py

--- api/strava_api/refresh_access_token.py
+++ api/strava_api/refresh_access_token.py
@@ -1,8 +1,10 @@
-import requests
+##This script is to test the device to strava api connection 
+##If the connection is successful, your strava account information should display on the console
+#import requests
 
-CLIENT_ID = "201696"            # same as before
-CLIENT_SECRET = "633f43ca5566dc20c8ba22cb10668f193a381cb8"    # same as before
-REFRESH_TOKEN = "410bb7b5c879a1b4aeb0f52cb55deaab9006d964"    # from the previous 200 response
+CLIENT_ID = "201696"            # your strava user ID - in this case it's cammy's
+CLIENT_SECRET = "633f43ca5566dc20c8ba22cb10668f193a381cb8"    # strava secret client
+REFRESH_TOKEN = "410bb7b5c879a1b4aeb0f52cb55deaab9006d964"    # this should be updated based on the refresh_access_token script
 
 def main():
     url = "https://www.strava.com/api/v3/oauth/token"
@@ -19,3 +21,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
