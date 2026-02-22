ffd937c52d4ae1fbac55b480d5a4cf4d4860fc52
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-22 16:05:19 +0000

    Update test_strava_api.py

--- api/strava_api/test_strava_api.py
+++ api/strava_api/test_strava_api.py
@@ -1,6 +1,8 @@
-import requests
+##This script is to test the device to strava api connection 
+##If the connection is successful, your strava account information should display on the console
+#import requests
 
-ACCESS_TOKEN = "95ece59d960098fe62ecbff2c03f4e211150f700"
+ACCESS_TOKEN = "95ece59d960098fe62ecbff2c03f4e211150f700" # The new refresh access token code from running the refresh_access_token script should go here
 
 def main():
     headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
@@ -11,4 +13,5 @@ def main():
     print("Response JSON:", response.json())
 
 if __name__ == "__main__":
+
     main()
