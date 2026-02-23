73f1f5244f19853360103614628ca5c9790ef3f7
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-23 14:37:30 +0000

    Add files via upload

---
+++ imports.py
@@ -0,0 +1,7 @@
+#installing the rest of the dependencies into our environment
+
+from flask import Flask, jsonify, render_template, request
+#from  psycopg2
+import psycopg2
+#from python
+import time, os, requests
