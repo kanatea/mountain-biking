db220deae62048400f4735454c65a7cd6c74eeb4
Kana Tateishi <Kana.Tateishi22@myhunter.cuny.edu>
2026-02-23 17:42:46 +0000

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,7 +1,10 @@
 #installing the rest of the dependencies into our environment
 
+# for api
 from flask import Flask, jsonify, render_template, request
-#from  psycopg2
+#from  psycopg2 - for api
 import psycopg2
-#from python
+#from python - for strava api and etl
 import time, os, requests
+#from datetime - for timestamps
+from datetime import datetime, timedelta
